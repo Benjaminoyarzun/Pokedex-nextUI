cd3dc92e768028d46465a5db725bf0a12db58eac
Benjaminoyarzun <104220399+Benjaminoyarzun@users.noreply.github.com>
2022-08-19 14:05:28 -0300

    Add files via upload

---
+++ components/ui/Navbar.js
@@ -0,0 +1,31 @@
+import {Link, Text} from "@nextui-org/react";
+import React from 'react'
+import Image from "@nextui-org/react";
+import NextLink from "next/link" 
+const Navbar = () => {
+    return ( 
+        <div className="navbar-custom" style={}>
+          <image>
+            src=
+            alt="logo"
+          </image>
+            <link href="/">
+            <Text color="white" h2 sm={undefined}> P </Text>
+            <Text color="white" h3 sm={undefined}> okedex! </Text>
+
+
+        
+        
+        
+        <
+            /div>   
+     );
+}
+ 
+export default Navbar;
+<div>
+    link
+
+
+
+</div>   
